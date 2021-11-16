b5088b74c109737bbbf8823363a9a62d970b8765
reymundvirtus <reymundvirtus@outlook.com>
2021-11-17 07:39:14 +0800

    changes to static files

--- resources/views/subnet_calculator.blade.php
+++ resources/views/subnet_calculator.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" href="{{ url('css/subnet_css.css') }}">
+    <link rel="stylesheet" href="/css/subnet_css.css">
     <title>Subnet Calculator</title>
 </head>
 <body>
@@ -116,6 +116,6 @@
 
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
-    <script type="text/javascript" src="{{ url('js/subnet_js.js') }}"></script>
+    <script type="text/javascript" src="js/subnet_js.js"></script>
 </body>
 </html>
