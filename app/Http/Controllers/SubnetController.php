fa7cd6cc6e506c59fc209f2e144461d8c53a50b7
reymundvirtus <reymundvirtus@outlook.com>
2021-11-17 06:44:54 +0800

    changes to the controller

---
+++ app/Http/Controllers/SubnetController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SubnetController extends Controller
+{
+    public function index(){
+        return view('subnet_calculator');
+    }
+}
