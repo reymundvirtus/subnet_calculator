f785b8e62ad470aa17090021359905721e706030
reymundvirtus <reymundvirtus@outlook.com>
2021-11-17 08:03:37 +0800

    image add

--- resources/views/subnet_calculator.blade.php
+++ resources/views/subnet_calculator.blade.php
@@ -101,7 +101,7 @@
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
-                    <img src="{{ url('img/table.jpg') }}" alt="table">
+                    <img src="/img/table.jpg" alt="table">
                 </div>
             </div>
         </div>
